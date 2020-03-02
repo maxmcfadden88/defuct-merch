e0e2c0d86a07f14a8accc44a7240b77e24994ef0
github username <github email address>
2020-03-02 15:51:40 -0600

    seeds

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,20 @@
 class ProductsController < ApplicationController
      def show
         product = Product.find(params[:id])
+       
         render json: ProductSerializer.new(product)
     end
     
     def index
         products = Product.all
-        render json: ProductSerializer.new(product)
+        
+        # render json: ProductSerializer.new(products)
+        render json: products
     end
 end
+
+
+
+
+
 
