561e48ef5d75681c82b15912a401f6668f2ed0fe
github username <github email address>
2020-03-03 09:05:49 -0600

    user seeds

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        
+        # render json: ProductSerializer.new(products)
+        render json: users
+    end
+
+    def create
+        user = User.create(username: params[:username])
+        render json: user
+        
+    end
+
+
+
+
 end
